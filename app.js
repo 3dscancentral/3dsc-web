3a3ff8c7d916976cd9a36bf326c05cbc1731a280
ross creighton <rosson.creighton@gmail.com>
2017-01-08 16:38:25 -0500

    adds an html index page

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('home page'));
-app.get('/start-scan', (req, res) => res.send('start a scan'));
+app.get('/', (req, res) => {
+  res.sendfile('index.html', {root: __dirname })
+});
 
 export default app;
